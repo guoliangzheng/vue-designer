1300bfcb5ccbd0afad54d43b8521f288a90049fa
zgl <1067430049@qq.com>
2018-12-02 09:27:57 +0800

    first

---
+++ src/components/Draggable.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+    <div>
+</template>
+
+<script>
+export default {
+    props:{
+       resizeHorizontal:{
+           type:Boolean,
+       },
+       resizeVertical:{
+           type:Boolean,
+       },
+       scale:{
+           type:Number,
+       },
+       onResize:{
+           type:Function
+       },
+       onResizeStart:{
+           type:Function
+       },
+       getSize:{
+           type:Function
+       }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
+
